c4a0f6c812afeec434478b2b93c8c76b6bedc038
manish singh <pratapmanish63@gmail.com>
2025-01-03 19:48:22 +0530

    patter second

--- day3.js
+++ day3.js
@@ -17,10 +17,47 @@
 // console.log(pattern)
 // }
 
-for (let j = 1; j <= 5; j++) {
-  let pattern = "";
-  for (let i = 1; i <= j; i++) {
-    pattern += "*" + " ";
-  }
-  console.log(pattern);
-}
+// for (let j = 1; j <= 5; j++) {
+//   let pattern = "";
+//   for (let i = 1; i <= j; i++) {
+//     pattern += "*" + " ";
+//   }
+//   console.log(pattern);
+// }
+
+// 47- Right Triangle - Number
+// 	1
+// 	1 2
+// 	1 2 3
+// 	1 2 3 4
+// 	1 2 3 4 5
+
+// for(let j =1; j<=5; j++){
+//     let pattern = ""
+//     for (let i =1 ; i<=j;i++){
+//         pattern += i + ""
+//     }
+//     console.log(pattern)
+// }
+
+// 48- Right Triangle - Alphabet
+// 	A
+// 	A B
+// 	A B C
+// 	A B C D
+// 	A B C D E
+
+// for (let j = 1; j <= 5; j++) {
+//   let pattern = "";
+//   for (let i = 65; i < 65 + j; i++) {
+//     pattern += String.fromCharCode(i) + " ";
+//   }
+//   console.log(pattern);
+// }
+
+// 49- Inverted Right Triangle
+// 	* * * * *
+// 	* * * *
+// 	* * *
+// 	* *
+// 	*
