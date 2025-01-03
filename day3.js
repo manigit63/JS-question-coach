5d9efcad7d430035151f444d69cdc24d6c318d0e
manish singh <pratapmanish63@gmail.com>
2025-01-03 19:34:18 +0530

    pattern question

--- day3.js
+++ day3.js
@@ -1,18 +1,26 @@
 // 46- Right Triangle - Star
-// 	*
-// 	* *
-// 	* * *
-// 	* * * *
-// 	* * * * *
+// 	*         //  1
+// 	* *       //
+// 	* * *     //
+// 	* * * *   //
+// 	* * * * * //
 
 // outer loop represent : row
 //  inner loop represent : column
 
-for (let j =1 ; j<=5;j++){
-    let pattern =""
-    for (let i=1; i<=j;i++){
-        pattern += '*'+ " "
-    }
+// for (let j =1 ; j<=5;j++){
+//     let pattern =""
+//     for (let i=1; i<=j;i++){
+//         pattern += '*'+ " "
+//     }
 
-console.log(pattern)
+// console.log(pattern)
+// }
+
+for (let j = 1; j <= 5; j++) {
+  let pattern = "";
+  for (let i = 1; i <= j; i++) {
+    pattern += "*" + " ";
+  }
+  console.log(pattern);
 }
